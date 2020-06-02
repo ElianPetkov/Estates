d293c731a2efd865168db88bdc1a119d474e2ebf
ElianPetkov <elianpetkov1@gmail.com>
2020-06-02 12:47:06 +0300

    fixing argument names in functions

--- agent.h
+++ agent.h
@@ -1,21 +1,17 @@
 #ifndef AGENT_H_AGENT
 #define AGENT_H_AGENT
 #include <iostream>
-#include "RegImot.h"
 class Agent
 {
-
     public:
-    virtual void print()const = 0;
-    //virtual Agent * Clone()const = 0;
+    virtual void print()const=0;
     virtual void PrintHousesFromLowestPrice()=0;
     virtual void PrintFlatsFromLowestPrice()=0;
     virtual void PrintEstatesFromLowestPrice()=0;
     virtual void PrintHouses()=0;
     virtual void PrintFlats()=0;
-    virtual void PrintByPriceRange(double price1,double price2)=0;
-    virtual void PrintEstatesByTown(char * Town)=0;
-    virtual void PrintBySpaceRange(double size1,double size2)=0;
-
+    virtual void PrintByPriceRange(double ,double)=0;
+    virtual void PrintEstatesByTown(char* Town)=0;
+    virtual void PrintBySpaceRange(double ,double)=0;
 };
 #endif
